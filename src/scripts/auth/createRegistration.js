bb745e31b3133be4dc1af8253c74dbd90d40b55d
Lauren Riddle <lari7132@gmail.com>
2019-11-13 12:52:47 -0600

    working on session storage

--- src/scripts/auth/createRegistration.js
+++ src/scripts/auth/createRegistration.js
@@ -2,21 +2,25 @@
 export default () => {
 	// TO DO: Create registration html, which includes registration button.
 	return `
-		<h1 class="welcome">Register a New Account</h1>
+	<h1 class="welcome">Register a New Account</h1>
 	<form id="registerForm">
-	<section>
-		<label for="email-input">Email:</label>
-		<input type="email" name="email-input" id="email">
-	</section>
-	<section>
-		<label for="password-input-1">Password:</label>
-		<input type="password" name="password-input" id="password-1">
-	</section>
-	<section>
-		<label for="password-input-2">Password:</label>
-		<input type="password" name="password-input" id="password-2">
-	</section>
-</form>
-<button id="registerButton" class="dim pointer">Register</button>
+		<section>
+			<label for="name-input">Name:</label>
+			<input type="text" name="name-input" id="name">
+		</section>
+		<section>
+			<label for="email-input">Email:</label>
+			<input type="email" name="email-input" id="email">
+		</section>
+		<section>
+			<label for="password-input-1">Password:</label>
+			<input type="password" name="password-input" id="password-1">
+		</section>
+		<section>
+			<label for="password-input-2">Password:</label>
+			<input type="password" name="password-input" id="password-2">
+		</section>
+	</form>
+	<button id="registerButton" class="dim pointer">Register</button>
 	`;
 };
