daf58f639d23a27c64dd11151d8cef11897c7eaa
Manila Bui <manila.bui@gmail.com>
2019-11-14 01:05:47 -0600

    form styling

--- src/scripts/news/createForm.js
+++ src/scripts/news/createForm.js
@@ -3,55 +3,58 @@
 
 export default () => {
 	return `
-		<form id="articleForm">
-			<fieldset>
-				<label for="title" class="f6 b db mb2">Title</label>
+		<div class="article-form ph5 pt4 pb5 mt5 mr3 br3 shadow-1">
+			<h3>Create New Article</h3>
+			<form id="articleForm">
+				<fieldset>
+					<label for="title" class="f6 b db mb2">Title</label>
+					<input 
+						class="articleInput-title w-100" 
+						type="text" 
+						name="title" 
+						required 
+					/>
+				</fieldset>
+				<fieldset>
+					<label for="synopsis" class="f6 b db mb2">Synopsis</label>
+					<textarea 
+						class="articleInput-synopsis w-100"
+						name="synopsis"
+						placeholder="Tell me bout it"
+						required
+					></textarea>
+				</fieldset>
+				<fieldset>
+					<label for="url" class="f6 b db mb2">Url</label>
+					<input 
+						class="articleInput-url w-100"
+						type="text"
+						name="url"
+						required
+					/>
+				</fieldset>
 				<input 
-					class="articleInput-title" 
-					type="text" 
-					name="title" 
-					required 
+					class="input-id" 
+					type="number" 
+					name="id" 
+					hidden
 				/>
-			</fieldset>
-			<fieldset>
-				<label for="synopsis" class="f6 b db mb2">Synopsis</label>
-				<textarea 
-					class="articleInput-synopsis"
-					name="synopsis"
-					placeholder="Tell me bout it"
-					required
-				></textarea>
-			</fieldset>
-			<fieldset>
-				<label for="url" class="f6 b db mb2">Url</label>
-				<input 
-					class="articleInput-url" 
-					type="text" 
-					name="url" 
-					required 
-				/>
-			</fieldset>
+			</form>
 			<input 
-				class="input-id" 
-				type="number" 
-				name="id" 
+				class="button-save f6 fw5 bg-white orange hover-white hover-bg-green pointer pa2 pv1 mt2 mb3 mr3 fr br2"
+				form="articleForm"
+				type="submit" 
+				name="saveArticle" 
+				value="Save"
+			/>
+			<input 
+				class="button-update f6 pv1 ph3 mv3 mr3 fr br2"
+				form="articleForm"
+				type="submit" 
+				name="updateArticle" 
+				value="Update"
 				hidden
 			/>
-		</form>
-		<input 
-			class="button-save"
-			form="articleForm"
-			type="submit" 
-			name="saveArticle" 
-			value="Save"
-		/>
-		<input 
-			class="button-update"
-			form="articleForm"
-			type="submit" 
-			name="updateArticle" 
-			value="Update"
-			hidden
-		/>
+		</div>
 	`;
 };
