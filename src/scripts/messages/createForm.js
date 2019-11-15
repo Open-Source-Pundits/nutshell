9dfa59f15a5be64a7115a0588fbdf249ed7038f5
Manila Bui <manila.bui@gmail.com>
2019-11-15 13:34:51 -0600

    styled message to different message from sender

--- src/scripts/messages/createForm.js
+++ src/scripts/messages/createForm.js
@@ -16,7 +16,7 @@ export default {
     makeMessageComponent: (entry) => {
         return ` 
         <div class="messageContainer-${entry.id}">
-        <h3 id="messages-${entry.id}"><span id="friends-${entry.user.id}">${entry.user.username}</span> said: ${entry.message}</h3>
+        <h3 id="messages-${entry.id}"><span id="friends-${entry.user.id}">${entry.user.username}</span> said: <span class="fw4">${entry.message}</span></h3>
         </div>
         `
 //adding a timestamp to the message with  at ${entry.timestamp}
